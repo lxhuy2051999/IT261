ed7bed9f67971d65386fd9a0d867e05e866bbc0e
Huy Xuon Lieu <49007183+lxhuy2051999@users.noreply.github.com>
2021-12-07 00:54:47 -0800

    Add files via upload

--- it261/website/gallery.php
+++ it261/website/gallery.php
@@ -2,25 +2,6 @@
 session_start();
 include 'config.php';
 
-if(!isset($_SESSION['username'])){
-    $_SESSION['msg'] = 'You  must login first';
-    header('Location: login.php');
-}
-
-if(isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['username']);
-    header('Location: login.php');
-}
-
- if(isset($_SESSION['success'])) {
-    unset($_SESSION['success']);
-}
-?>
-<?php if(isset($_SESSION['username'])) : ?>
-<?php include('./includes/header.php'); ?>
-
-<?php include 'gallery-config.php';
 function create_table_cell($html = '', $attributes = array()){
     $attributes_str = '';
     foreach($attributes as $key => $value){
@@ -54,6 +35,23 @@ function create_table_header(){
     $result .=  '</tr>';
     return $result;
 }
+
+if(!isset($_SESSION['username'])){
+    $_SESSION['msg'] = 'You  must login first';
+    header('Location: login.php');
+}
+
+if(isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location: login.php');
+}
+
+ if(isset($_SESSION['success'])) {
+    unset($_SESSION['success']);
+}
+if(isset($_SESSION['username'])) : 
+    
 
 $sql = "SELECT * FROM languages";
 $iConn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
